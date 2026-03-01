0b8c301e47d574d7b621550d2915f257e8bb3789
Kentaa Kakashi <168970257+Kentaakakashi@users.noreply.github.com>
2026-03-01 14:01:42 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   server: {
-    host: true,
+    host: "0.0.0.0",
     port: 3000,
-    allowedHosts: "all",
+    strictPort: true,
+    allowedHosts: ["all"],
   },
   plugins: [react()],
 });
