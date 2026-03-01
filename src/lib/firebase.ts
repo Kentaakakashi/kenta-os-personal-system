315ee4b6b3ac3d0d0b8af548c7bb4bdd1f18e016
Kentaa Kakashi <168970257+Kentaakakashi@users.noreply.github.com>
2026-03-01 18:57:37 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,11 +1,7 @@
-// Firebase core
 import { initializeApp } from "firebase/app";
-
-// Firebase services we actually need
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your config (using what you sent)
 const firebaseConfig = {
   apiKey: "AIzaSyCEthECHBhYBdIWmbaAQMJDyIUxn20TWiQ",
   authDomain: "pooka-os.firebaseapp.com",
@@ -13,12 +9,10 @@ const firebaseConfig = {
   storageBucket: "pooka-os.firebasestorage.app",
   messagingSenderId: "304292975845",
   appId: "1:304292975845:web:d1441057dfb5a706bbb00c",
-  measurementId: "G-8V6H5JXB4Q"
+  measurementId: "G-8V6H5JXB4Q",
 };
 
-// Initialize app
 const app = initializeApp(firebaseConfig);
 
-// Export auth + db
 export const auth = getAuth(app);
 export const db = getFirestore(app);
